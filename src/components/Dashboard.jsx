b957e866ce3eb1de10612427a66ba66d3497dbb6
zeqirie <erblin.zeqiri17@gmail.com>
2024-05-23 23:08:05 +0200

    Ajout de burger pour formulaire, deco, et fiche de paie

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,79 +5,109 @@ import { firebaseConfig } from "../providers/firebaseConfig";
 import Form from "./Form";
 import { getDatabase, ref, get } from "firebase/database";
 import DisplayElements from "./DisplayElements";
+import "./Dashboard.css";
 
 const Dashboard = () => {
-    const firebase = new firebaseConfig();
-    const [user, setUser] = useState(null);
-    const [app, setApp] = useState(firebase.getApp());
-    const [form, setform] = useState(false);
-    const navigate = useNavigate();
+  const firebase = new firebaseConfig();
+  const [user, setUser] = useState(null);
+  const [app, setApp] = useState(firebase.getApp());
+  const [form, setForm] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const auth = getAuth(app);
-        const db = getDatabase(app);
-        
-        const unsubscribe = onAuthStateChanged(auth, async (user) => {
-            if (user) {
-                const userRef = ref(db, `users/${user.uid}`);
-                const snapshot = await get(userRef);
-                
-                if (snapshot.exists()) {
-                    const userData = snapshot.val();
-                    const { entrepriseId } = userData;
-                    const name = user.displayName.split(" ")[0];
-                    const firstname = user.displayName.split(" ")[1];
+  useEffect(() => {
+    const auth = getAuth(app);
+    const db = getDatabase(app);
 
-                    const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-                    const capitalizedFirstName = firstname.charAt(0).toUpperCase() + firstname.slice(1).toLowerCase();
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userRef = ref(db, `users/${user.uid}`);
+        const snapshot = await get(userRef);
 
-                    setUser({
-                        uid: user.uid,
-                        capitalizedName,
-                        capitalizedFirstName,
-                        entrepriseId,
-                        app: app
-                    });
-                } else {
-                    console.log("No data available");
-                }
-            } else {
-                navigate("/");
-            }
-        });
+        if (snapshot.exists()) {
+          const userData = snapshot.val();
+          const { entrepriseId } = userData;
+          const [name, firstname] = user.displayName.split(" ");
 
-        // Cleanup subscription on unmount
-        return () => unsubscribe();
-    }, [app, navigate]);
+          const capitalizedName =
+            name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+          const capitalizedFirstName =
+            firstname.charAt(0).toUpperCase() +
+            firstname.slice(1).toLowerCase();
 
-    const openModal = () => {
-        setform(true);
-    };
+          setUser({
+            uid: user.uid,
+            capitalizedName,
+            capitalizedFirstName,
+            entrepriseId,
+            app: app,
+          });
+        } else {
+          console.log("No data available");
+        }
+      } else {
+        navigate("/");
+      }
+    });
 
-    const closeModal = () => {
-        setform(false);
-    };
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [app, navigate]);
 
-    const logout = () => {
-        const auth = getAuth(app);
-        auth.signOut().then(() => {
-            navigate("/");
-        });
-    }
+  const openModal = () => {
+    setForm(true);
+  };
 
-    return (
-        <>
-        <div className="displayElements">
-            {user && <h1>Bonjour {user.capitalizedName} {user.capitalizedFirstName}</h1>}
-            <button onClick={logout}>Deconnexion</button>
-            <br />
-            <button onClick={openModal}>Ouvrir le formulaire</button>
-            {form && <Form user={user} onClose={closeModal} />}
-            <br />
-            {user && <DisplayElements user={user}/>}
+  const closeModal = () => {
+    setForm(false);
+  };
+
+  const logout = () => {
+    const auth = getAuth(app);
+    auth.signOut().then(() => {
+      navigate("/");
+    });
+  };
+
+  return (
+    <>
+      <div className="displayElements">
+        {user && (
+          <h1>
+            Bonjour {user.capitalizedName} {user.capitalizedFirstName}
+          </h1>
+        )}
+        <div className="menu-container">
+          <input id="check" type="checkbox" className="menu-checkbox" />
+          <label htmlFor="check" className="menuButton">
+            <span className="top"></span>
+            <span className="mid"></span>
+            <span className="bot"></span>
+          </label>
+          <div className="menu-options">
+            <ul>
+              <li>
+                <a href="#option3">Fiche de salaire</a>
+              </li>
+              <li>
+                <a href="#" onClick={openModal}>
+                  Ouvrir le formulaire
+                </a>
+              </li>
+              <li>
+                <a href="#" onClick={logout}>
+                  Deconnexion
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-        </>
-    );
-}
+        <br />
+        {form && <Form user={user} onClose={closeModal} />}
+        <br />
+        {user && <DisplayElements user={user} />}
+      </div>
+    </>
+  );
+};
 
 export default Dashboard;
